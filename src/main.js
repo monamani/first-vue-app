09da0340d20fcfe34b7019bc2461670bbca23926
monamani <manimona.k@gmail.com>
2023-03-03 10:44:01 +0400

    add project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 createApp(App).mount('#app')

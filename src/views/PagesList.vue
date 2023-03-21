c9c6ac33e5404942f908c8691eafeb99354675e0
monamani <manimona.k@gmail.com>
2023-03-21 12:59:55 +0400

    setup part

--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -1,21 +1,42 @@
 <template>
   <div class="container">
-    <p>{{counter}}</p>
-    <button @click.prevent="click">click here</button>
+    <p>{{data.counter}}</p>
+    <button @click.prevent="clickReactive">click here</button>
   </div>
 </template>
 
-<script>
-  export default {
-    data(){
-      return {
-        counter: 1
-      }
-    },
-   methods:{
-    click(){
-        this.counter++;
-    }
-  },
-  }
+<script setup>
+// SETUP for VS 3 will use setup iunstead of function setup with different params on vue2
+// need to tell that value is reactive so will be change it on the page
+import {ref, reactive} from 'vue';
+// With reactive object or data
+const data = reactive({counter:0}) // will use reactive when it s object , data
+function clickReactive(){
+  //counter++;
+ // will change it to counter.value
+  data.counter++;
+  console.log(counter)
+}
+
+// with ref example
+let counter = ref(0); // {value:0}
+function clickRef(){
+  //counter++;
+ // will change it to counter.value
+  counter.value++;
+  console.log(counter)
+}
+// THis is one solution for click btn 
+  // export default {
+  //   data(){
+  //     return {
+  //       counter: 1
+  //     }
+  //   },
+  //  methods:{
+  //   click(){
+  //       this.counter++;
+  //   }
+  // },
+  // }
 </script>

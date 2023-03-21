9566367bf842ab0f4c3b621b4d9b11fae3f4c8b7
monamani <manimona.k@gmail.com>
2023-03-21 13:38:57 +0400

    method 2 by using route instead of props

--- src/views/PageEdit.vue
+++ src/views/PageEdit.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="container">
         <hr>
-        <h2>Edit Page {{props.index}}</h2>
+        <h2>Edit Page {{props.index}} {{route.params.index}}</h2>
     </div>
 </template>
 
 <script setup>
-    // we will pass by props
-    const props=defineProps(['index']);
+import { useRoute } from 'vue-router';
+// Method 2 using the route params and remove "prpos true from routes"
+const route  =useRoute(); 
+//Method 1 we will pass by props
+const props=defineProps(['index']);
 </script>
